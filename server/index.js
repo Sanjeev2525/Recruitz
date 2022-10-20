6d9aaf250a0af0e628b17bca22786d147325c672
Sanjeev <sanjeeganesh25@gmail.com>
2022-10-20 12:15:23 +0530

    Routes added

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/post.js';
+
+const app = express();
+
+app.use('/HELLO', postRoutes);
+
+app.use(bodyParser.json({limit: "30mb",extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb",extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://sanjeev:sanjeev@cluster0.5uxta5t.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => {console.log(`Server Running on port  : ${PORT}`)}));
